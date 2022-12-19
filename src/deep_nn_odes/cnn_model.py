3a6b7e56eed7ef2cb279b75e8898915e2cca403c
eikehmueller <eike.h.mueller@gmail.com>
2022-12-19 14:44:29 +0100

    Remove old CNN model

--- src/deep_nn_odes/cnn_model.py
+++ src/deep_nn_odes/cnn_model.py
@@ -1,40 +1,12 @@
 """CNN for image classification
 
-Implements two simple CNN models for image classification.
+Implements simplified ResNet model for image classification.
 
 The input is an array of the shape (n_images,32,32,C), where each image has width h_x 
 and height n_y and has C channels.
 
 The model output are the logits of the class probabilities, i.e. an array of shape
 (n_images,M) where M is the number of classes.
-
-The first VGG-like model consists of the following layers:
-
-    CNN Superlayer 0
-      Convolution 0, kernelsize = (3,3) output channels = 16
-      RELU nonlinearity
-      Convolution 1, kernelsize = (3,3) output channels = 16
-      RELU nonlinearity
-      MaxPool2D, kernelsize (2,2) and stride (2,2)
-    CNN Superlayer 1
-      Convolution 2, kernelsize = (3,3) output channels = 32
-      RELU nonlinearity
-      Convolution 3, kernelsize = (3,3) output channels = 32
-      RELU nonlinearity
-      MaxPool2D, kernelsize (2,2) and stride (2,2)
-    CNN Superlayer 2
-      Convolution 4, kernelsize = (3,3) output channels = 64
-      RELU nonlinearity
-      Convolution 5, kernelsize = (3,3) output channels = 64
-      RELU nonlinearity
-      MaxPool2D, kernelsize (2,2) and stride (2,2)
-    Dense layers
-      Flattening from (4,4,64) to 1024 (for 32x32 input images)
-      Dense RELU layer from 1024 to 64
-      Dense layer from 64 to M to compute logits
-
-The second model is a ResNet model
-
 """
 
 from functools import partial
@@ -43,57 +15,6 @@ from jax import numpy as jnp
 from jax import random
 from jax import lax
 from flax.linen import max_pool, avg_pool
-
-
-def init_cnn_parameters(input_channels=3, n_categories=10):
-    """Initialise parameters for simple CNN
-
-    :arg key: key for random number generation
-    :arg input_channels: number C of input channels
-    :arg n_categories: number M of output categories
-    """
-
-    from collections import defaultdict
-
-    seed = 47
-    key = random.PRNGKey(seed)
-    params = defaultdict(list)
-    # weights and biases for CNN layers
-    for C_in, C_out in (
-        (input_channels, 16),
-        (16, 16),
-        (16, 32),
-        (32, 32),
-        (32, 64),
-        (64, 64),
-    ):
-        scale = jnp.sqrt(2 / C_in)
-        key, subkey = random.split(key)
-        params["cnn_weights"].append(
-            random.uniform(
-                subkey,
-                (3, 3, C_in, C_out),
-                minval=-scale,
-                maxval=scale,
-                dtype=jnp.float32,
-            )
-        )
-        params["cnn_biases"].append(jnp.ones((C_out,), dtype=jnp.float32))
-    # weights and biases of intermediate dense layer
-    scale = jnp.sqrt(2 / 1024)
-    key, subkey = random.split(key)
-    params["dense_weights"] = random.uniform(
-        subkey, (1024, 64), minval=-scale, maxval=scale, dtype=jnp.float32
-    )
-    params["dense_biases"] = jnp.ones((64,), dtype=jnp.float32)
-    # weights and biases of dense classification layer
-    key, subkey = random.split(key)
-    scale = jnp.sqrt(2 / 64)
-    params["classification_weights"] = random.uniform(
-        subkey, (64, n_categories), minval=-scale, maxval=scale, dtype=jnp.float32
-    )
-    params["classification_biases"] = jnp.ones((n_categories,), dtype=jnp.float32)
-    return params
 
 
 def dropout(input, state, p_dropout):
@@ -122,42 +43,6 @@ def dropout(input, state, p_dropout):
         lambda x: x,
         input,
     )
-
-
-def cnn_model(params, state, x):
-    """Evaluate the simple CNN model
-
-    :arg params: Model parameters (initialised with init_cnn_parameters())
-    :arg state: Model state variable. Required to implement dropout.
-    :arg x: input image of shape (n_images,32,32,C)
-    """
-    batch_size = x.shape[0]
-    # CNN layers
-    for super_layer in range(3):
-        for sub_layer in range(2):
-            x = (
-                lax.conv_general_dilated(
-                    x,
-                    params["cnn_weights"][2 * super_layer + sub_layer],
-                    (1, 1),
-                    "SAME",
-                    dimension_numbers=("NHWC", "HWIO", "NHWC"),
-                )
-                + params["cnn_biases"][2 * super_layer + sub_layer]
-            )
-            x = jax.nn.relu(x)
-        x = max_pool(x, (2, 2), strides=(2, 2))
-
-    # Dense layers
-    x = jnp.reshape(x, (batch_size, -1))
-    # Dropout layer (commented out for now)
-    p_dropout = state["p_dropout"]
-    x = dropout(x, state, p_dropout)
-    # RELU layer
-    x = jax.nn.relu(x @ params["dense_weights"] + params["dense_biases"])
-    x = dropout(x, state, p_dropout)
-    # Compute logits
-    return x @ params["classification_weights"] + params["classification_biases"]
 
 
 def init_residualblock_parameters(key, input_channels, n_conv=2):
