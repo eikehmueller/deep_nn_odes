3264d199e9d7f7eac26f7708fed8cc6deca3d1bf
Eike Mueller <eike.h.mueller@gmail.com>
2023-01-18 10:39:28 +0000

    Fixed the model to be consistent with the paper; use a longer integration period

--- src/deep_nn_odes/model.py
+++ src/deep_nn_odes/model.py
@@ -40,9 +40,9 @@ def init_hamiltonian_parameters(dim, n_class, n_steps=8):
 
     Returns a dictionary with weights and biases for the following components:
 
-    * (K^x_j,b^x_j) and (K^p_j,b^p_j) for the j-the Verlet update step,
-        where j=0,1,...,n-1; the corresponding keys in the dictionary are "K_x", "b_x",
-        "K_p" and "b_p"; K^*_j are d x d matrices and b^* are scalars.
+    * (K_j,b_j) for the j-the Verlet update step,
+        where j=0,1,...,n-1; the corresponding keys in the dictionary are "K" and "b";
+        K_j are d x d matrices and b_j are scalars.
     * (K,b) for the final classification step, the corresponding key
         is "classification". K is a n_c x d matrix and b is a n_c vector.
 
@@ -50,10 +50,8 @@ def init_hamiltonian_parameters(dim, n_class, n_steps=8):
     :arg n_class: number of classes n_c
     """
     params = dict(
-        K_x=np.random.normal(size=(n_steps, dim, dim)) * np.sqrt(2 / dim),
-        b_x=np.ones(shape=(n_steps,)),
-        K_p=np.random.normal(size=(n_steps, dim, dim)) * np.sqrt(2 / dim),
-        b_p=np.ones(shape=(n_steps,)),
+        K=np.random.normal(size=(n_steps, dim, dim)) * np.sqrt(2 / dim),
+        b=np.ones(shape=(n_steps,)),
         classification=dict(
             weights=np.random.normal(size=(dim, n_class)) * np.sqrt(2 / dim),
             biases=np.ones(shape=(n_class,)),
@@ -68,19 +66,19 @@ def hamiltonian_model(params, x, n_steps=8):
     starting with x_0 = x, evolve the state forward in
     time according to the Verlet integration
 
-        p_0 = h/2*K_p.sigmoid(x_0.K^p_0+b_p)
+        p_0 = - h/2*K.sigmoid(x_0.K_0+b)
 
     and
 
-        x_{j+1} = x_j - h*K_x.sigmoid(p_j.K^x_j+b^x_j)
-        p_{j+1} = p_j + h*K_p.sigmoid(x_j.K^p_{j+1}+b^p_{j+1})
+        x_{j+1} = x_j + h*K.sigmoid(p_j.K_j+b_j)
+        p_{j+1} = p_j - h*K.sigmoid(x_j.K_{j+1}+b_{j+1})
 
     for j = 0,...,n-1.
     (note that this assumes that the initial momentum is zero)
 
     The logits are obtained from x_n as
 
-        logits = x_n.K + b
+        logits = x_n.K_{class} + b_{class}
 
     :arg params: model parameters, as created with init_mlp_parameters()
     :arg x: input state, tensor of shape (n_data,d)
@@ -88,23 +86,25 @@ def hamiltonian_model(params, x, n_steps=8):
 
     returns logits
     """
-    h = 1.0 / n_steps
-    K_x, b_x = params["K_x"], params["b_x"]
-    K_p, b_p = params["K_p"], params["b_p"]
+    h = 20.0 / n_steps
+    K, b = params["K"], params["b"]
     activation = jax.nn.sigmoid
-    p = 0.5 * h * activation(x @ K_p[0, :, :] + b_p[0]) @ K_p[0, :, :].T
+    p = -0.5 * h * activation(x @ K[0, :, :] + b[0]) @ K[0, :, :].T
     for j in range(n_steps):
-        x -= h * activation(p @ K_x[j, :, :] + b_x[j]) @ K_x[j, :, :].T
+        x += h * activation(p @ K[j, :, :] + b[j]) @ K[j, :, :].T
         if j < n_steps - 1:
-            p += h * activation(x @ K_p[j + 1, :, :] + b_p[j + 1]) @ K_p[j + 1, :, :].T
-    K, b = params["classification"]["weights"], params["classification"]["biases"]
-    return x @ K + b
+            p -= h * activation(x @ K[j + 1, :, :] + b[j + 1]) @ K[j + 1, :, :].T
+    K, b = (
+        params["classification"]["weights"],
+        params["classification"]["biases"],
+    )
+    return p @ K + b
 
 
 def hamiltonian_regulariser(params, alpha=1.0e-3):
     """Regulariser for Hamiltonian formulation
 
-    Returns the sum R(K_x) + R(K_p) + r(b_x) + r(b) where
+    Returns the sum R(K) + r(b) where
 
         R(K) = 1/(2*h) sum_{j=1}^{n-1} ||K_j-K_{j-1}||_F^2
 
@@ -121,9 +121,7 @@ def hamiltonian_regulariser(params, alpha=1.0e-3):
     :arg params: model parameters, as created by init_hamiltonian_parameters()
     :arg alpha: scaling factor
     """
-    n_steps = float(params["K_x"].shape[0])
-
-    R = lambda A: 0.5 / n_steps * jnp.sum((A[1:, ...] - A[:-1, ...]) ** 2)
-    return alpha * (
-        R(params["K_x"]) + R(params["K_p"]) + R(params["b_x"]) + R(params["b_p"])
-    )
+    n_steps = float(params["K"].shape[0])
+    h = 20.0 / n_steps
+    R = lambda A: 0.5 * h * jnp.sum((A[1:, ...] - A[:-1, ...]) ** 2)
+    return alpha * (R(params["K"]) + R(params["b"]))
